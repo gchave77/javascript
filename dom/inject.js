153d11fcc9951d41b45e5e2c0f00a82034b07863
gabe chavez <gabe.chavez77@gmail.com>
2020-05-31 21:36:12 -1000

    link html with js

--- dom/inject.js
+++ dom/inject.js
@@ -1,3 +1,27 @@
+// Exercise 2:
 /*Exercise instructions : take the html code from the following codepen : https://codepen.io/ayunas-the-scripter/pen/WNQgpqB, and use JavaScript DOM manipulation to inject the html into the inject id element of inject.html
 */
+// using the inject.js file, write some vanilla javascript to inject the entire contents of the html into the <div id="inject"> <div> html element. 
+// You will have to link together the inject.html with the inject.js in order to get this to work. 
+// You will need to reserach Javascript DOM manipulation in order to complete this exercise. 
+// You are NOT allowed to copy & paste in any html directly in the inject.html document.
 
+
+/*
+<center>
+  <h1><i>HTML Practice Exercise TEKcamp.</i></h1>
+  <h2><a href="">TEKsystems "TEKcamp"</a></h2>
+</center>
+
+<hr />
+<p>I love <i>HTML</i> for the following reasons:</p>
+<ol>
+  <li>I learned it quickly.</li>
+  <li>I can make web pages using code
+<li> It’s fun.</li>
+</ol>
+<hr />
+  <p>My instructor’s email address is: <a href="mailto:ayunas@teksystems.com">ayunas@teksystems.com</a>.</p>
+<h1>Have a great day!</h1>  I really look forward to learning how to code!  Have a great day.  
+-[Team "Git'er Done"]
+*/
