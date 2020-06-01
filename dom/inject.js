9f6ad04fcc16df7284a50d36fd0f4b152f4e26d8
gabe chavez <gabe.chavez77@gmail.com>
2020-05-31 22:49:18 -1000

    final inject.js update

--- dom/inject.js
+++ dom/inject.js
@@ -1,12 +1,3 @@
-// Exercise 2:
-/*Exercise instructions : take the html code from the following codepen : https://codepen.io/ayunas-the-scripter/pen/WNQgpqB, and use JavaScript DOM manipulation to inject the html into the inject id element of inject.html
-*/
-// using the inject.js file, write some vanilla javascript to inject the entire contents of the html into the <div id="inject"> <div> html element. 
-// You will have to link together the inject.html with the inject.js in order to get this to work. 
-// You will need to reserach Javascript DOM manipulation in order to complete this exercise. 
-// You are NOT allowed to copy & paste in any html directly in the inject.html document.
-
-
 /*
 <center>
   <h1><i>HTML Practice Exercise TEKcamp.</i></h1>
@@ -25,3 +16,25 @@
 <h1>Have a great day!</h1>  I really look forward to learning how to code!  Have a great day.  
 -[Team "Git'er Done"]
 */
+
+// Exercise 2:
+/*Exercise instructions : take the html code from the following codepen : https://codepen.io/ayunas-the-scripter/pen/WNQgpqB, and use JavaScript DOM manipulation to inject the html into the inject id element of inject.html
+*/
+// using the inject.js file, write some vanilla javascript to inject the entire contents of the html into the <div id="inject"> <div> html element. 
+// You will have to link together the inject.html with the inject.js in order to get this to work. 
+// You will need to reserach Javascript DOM manipulation in order to complete this exercise. 
+// You are NOT allowed to copy & paste in any html directly in the inject.html document.
+
+let html = '<center><h1><i>HTML Practice Exercise TEKcamp.</i></h1>';
+html += '<h2><a href="">TEKsystems "TEKcamp"</a></h2></center>';
+html += '<hr />';
+html += '<p>I love <i>HTML</i> for the following reasons:</p>';
+html += '<ol><li>I learned it quickly.</li><li>I can make web pages using code<li> It’s fun.</li></ol>';
+html += '<hr />';
+html += '<p>My instructor’s email address is: <a href="mailto:ayunas@teksystems.com">ayunas@teksystems.com</a>.</p>';
+html += '<h1>Have a great day!</h1>  I really look forward to learning how to code!  Have a great day.  -[Team "Git\'er Done"]';
+
+document.getElementById("inject").innerHTML = html;
+
+// https://stackoverflow.com/questions/3762385/best-way-to-inject-html-using-javascript
+// innerHTML made it easy to inject html code.
